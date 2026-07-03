062850c6cad4e2fa98cd4797f83cc2a6ec71c99f
DimAURA <dimauro.luca@studenti.salesianisesto.it>
2026-07-03 09:14:03 +0200

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'ricette-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dizionario_di_ricette.js',
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
